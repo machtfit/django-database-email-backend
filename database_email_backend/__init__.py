92378eb5de4c83cb2cf0360cc4ff1ff8b3cd8576
Thialfihar <thi@thialfihar.org>
2016-03-09 11:34:53 +0100

    Bump version

--- database_email_backend/__init__.py
+++ database_email_backend/__init__.py
@@ -1,4 +1,4 @@
 #-*- coding: utf-8 -*-
-VERSION = (0, 2, 1, 'machtfit', 2)
-__version__ = "0.2.1-machtfit-2"
+VERSION = (0, 2, 1, 'machtfit', 3)
+__version__ = "0.2.1-machtfit-3"
 __authors__ = ["Stefan Foulis <stefan.foulis@gmail.com>", ]
